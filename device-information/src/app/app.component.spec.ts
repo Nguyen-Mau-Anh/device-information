b3da0f0ffed255e2725b0e9d5e43938f404f5e80
AnhNM <twas.horus@gmail.com>
2024-12-15 22:57:23 +0700

    Change version

--- device-information/src/app/app.component.spec.ts
+++ device-information/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'device-status' title`, () => {
+  it(`should have the 'device-information' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('device-status');
+    expect(app.title).toEqual('device-information');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, device-status');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, device-information');
   });
 });
